ca419ac7dbc7b02187d464c5f0f93a3ac9f0b528
Jordan Woodruff <boogermanus@gmail.com>
2025-05-13 11:58:14 -0500

    add user station model

---
+++ ObserveWeather.Blazor/Core/Models/UserStation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using ObserveWeather.Blazor.Data;
+
+namespace ObserveWeather.Blazor.Core.Models;
+
+public class UserStation
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public int Id { get; set; }
+    public required string StationId { get; set; }
+    public required string UserId { get; set; }
+    public ApplicationUser? User { get; set; }
+    
+}
