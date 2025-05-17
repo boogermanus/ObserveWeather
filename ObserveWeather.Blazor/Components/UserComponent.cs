252a8ba35e39556e2b10b44c913ca2e9b47c0d1e
Jordan Woodruff <boogermanus@gmail.com>
2025-05-16 20:58:41 -0500

    user component

---
+++ ObserveWeather.Blazor/Components/UserComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ObserveWeather.Blazor.Components;
+
+public class UserComponent : ComponentBase
+{
+    [Inject]
+    protected IHttpContextAccessor HttpContextAccessor { get; set; } = null!;
+    
+}
