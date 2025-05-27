7424bd5345c3fca975652027664057edeb592f43
Jordan Woodruff <boogermanus@gmail.com>
2025-05-27 17:22:19 -0500

    add n/a string when data is not available

--- ObserveWeather.Blazor/Core/ApiModels/ObservationModel.cs
+++ ObserveWeather.Blazor/Core/ApiModels/ObservationModel.cs
@@ -8,13 +8,29 @@ public class ObservationModel
     public double DewpointF => ConvertToFahrenheit(Properties.Dewpoint.Value);
     public double WindSpeedMph => ConvertToMph(Properties.WindSpeed.Value);
     public double WindGustMph => ConvertToMph(Properties.WingGust.Value);
-    public double? PressureBar => Properties.BarometricPressure.Value / 100;
-
+    public double PressureBar =>
+        Properties.BarometricPressure.Value.HasValue ? Properties.BarometricPressure.Value.Value / 100 : 0;
+    public string? PressureBarString => PressureBar != 0 ? $"{PressureBar} mb" : null;
     public double PressureHgIn => Properties.BarometricPressure.Value.HasValue
         ? Math.Round(Properties.BarometricPressure.Value.Value / 3386, 2)
         : 0;
-    public double VisibilityMiles => Math.Round(Properties.Visibility.Value.Value / 1609, 2) ;
-    public double VisibilityKilometers => Properties.Visibility.Value.Value / 1000;
+    private string? PressureHgString => PressureBar != 0 ? $"{PressureHgIn} in" : null;
+    public string PressureString =>
+        Properties.BarometricPressure.Value.HasValue ? $"{PressureHgIn} ({PressureHgString})" : "n/a";
+
+    private double VisibilityMiles => Properties.Visibility.Value.HasValue
+        ? Math.Round(Properties.Visibility.Value.Value / 1609, 2)
+        : 0;
+    private string? VisibilityMilesString => VisibilityMiles != 0 ? $"{VisibilityMiles} mi" : null ;
+
+    private double VisibilityKilometers =>
+        Properties.Visibility.Value.HasValue ? Properties.Visibility.Value.Value / 1000 : 0;
+    private string? VisibilityKilometersString =>
+        VisibilityKilometers != 0 ? $"{VisibilityKilometers} km" : null;
+    public string VisibilityString => Properties.Visibility.Value.HasValue
+        ? $"{VisibilityMilesString} ({VisibilityKilometersString})"
+        : "n/a";
+
     public double WindChillF => ConvertToFahrenheit(Properties.WindChill.Value);
     public double WindChillC => Math.Round(Properties.WindChill.Value ?? 0, 2);
     public double HeatIndexF => ConvertToFahrenheit(Properties.HeatIndex.Value);
